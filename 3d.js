d8a8a66913ebf2a4897c83549ed68fe059735a0b
Ruslan Isaev <subfear@mail.ru>
2023-09-16 19:54:49 +0600

    fix of the fix

--- 3d.js
+++ 3d.js
@@ -2,6 +2,25 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 import {RoughnessMipmapper} from 'three/examples/jsm/utils/RoughnessMipmapper';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyBtpi-RetdHZN1Zec9N40Hs4LxWuUnZ0hs",
+authDomain: "hivaweb-4624b.firebaseapp.com",
+projectId: "hivaweb-4624b",
+storageBucket: "hivaweb-4624b.appspot.com",
+messagingSenderId: "639272553172",
+appId: "1:639272553172:web:c9b2314bafc8e93ef3c941",
+measurementId: "G-FP5K38DTNL"
+};
+
 let mouseX = 0, mouseY = 0;
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
